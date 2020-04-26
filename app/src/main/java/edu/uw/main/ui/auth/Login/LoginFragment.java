cd9592893cec7ea316964feb314ec4e9946669a1
Brian Huang <brianhuangoff@gmail.com>
2020-04-26 16:15:57 -0700

    Authentication for login fragment + adding dependencies.

--- app/src/main/java/edu/uw/main/ui/auth/Login/LoginFragment.java
+++ app/src/main/java/edu/uw/main/ui/auth/Login/LoginFragment.java
@@ -1,4 +1,4 @@
-package edu.uw.main.ui.auth;
+package edu.uw.main.ui.auth.Login;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -6,9 +6,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 //import edu.uw.tcss450.LoginFragmentDirections;
@@ -26,7 +30,7 @@ import static edu.uw.main.PasswordValidator.checkPwdSpecialChar;
  * A simple {@link Fragment} subclass.
  */
 public class LoginFragment extends Fragment {
-
+    private LoginViewModel mLoginModel;
     private FragmentLoginBinding binding;
     private PasswordValidator validate = checkPwdSpecialChar()
                                         .and(checkExcludeWhiteSpace())
@@ -38,6 +42,13 @@ public class LoginFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        mLoginModel = new ViewModelProvider(getActivity())
+                .get(LoginViewModel.class);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -46,6 +57,7 @@ public class LoginFragment extends Fragment {
         binding = FragmentLoginBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
@@ -53,6 +65,10 @@ public class LoginFragment extends Fragment {
                 processRegister());
         binding.buttonSuccess.setOnClickListener(button ->
                 processSuccess());
+
+        mLoginModel.addResponseObserver(
+                getViewLifecycleOwner(),
+                this::observeResponse);
     }
     public void processRegister(){
 
@@ -81,7 +97,7 @@ public class LoginFragment extends Fragment {
         pwdValidate.processResult(pwdValidate
                         .apply(binding.textPassword.getText().toString())
                         .filter(result -> result != ValidationResult.SUCCESS),
-                this::success,
+                this::verifyAuthWithServer,
                 this::handleSuccessError);
     }
     private void handleSuccessError(ValidationResult result) {
@@ -110,5 +126,34 @@ public class LoginFragment extends Fragment {
         return null;
     }
 
+    private void verifyAuthWithServer() {
+        mLoginModel.connect(
+                binding.textEmail.getText().toString(),
+                binding.textPassword.getText().toString());
+    }
+
+    /**
+     * An observer on the HTTP Response from the web server. This observer should be
+     * attached to SignInViewModel.
+     *
+     * @param response the Response from the server
+     */
+    private void observeResponse(final JSONObject response) {
+        if (response.length() > 0) {
+            if (response.has("code")) {
+                try {
+                    binding.textEmail.setError(
+                            "Error Authenticating: " +
+                                    response.getJSONObject("data").getString("message"));
+                } catch (JSONException e) {
+                    Log.e("JSON Parse Error", e.getMessage());
+                }
+            } else {
+                success();
+            }
+        } else {
+            Log.d("JSON Response", "No Response");
+        }
+    }
 
 }
