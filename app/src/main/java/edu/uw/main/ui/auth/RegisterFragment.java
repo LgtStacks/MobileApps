3b9c378b7249de7e25bab12e4c90d841ad0ecdc2
Gobindroop Mann <gobindroopm@gmail.com>
2020-04-27 19:42:15 -0700

    Added auth inside of the Register

--- app/src/main/java/edu/uw/main/ui/auth/RegisterFragment.java
+++ app/src/main/java/edu/uw/main/ui/auth/RegisterFragment.java
@@ -1,23 +1,28 @@
 package edu.uw.main.ui.auth;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import edu.uw.main.PasswordValidator;
 import edu.uw.main.databinding.FragmentRegisterBinding;
 
 import static edu.uw.main.PasswordValidator.checkClientPredicate;
 import static edu.uw.main.PasswordValidator.checkExcludeWhiteSpace;
+import static edu.uw.main.PasswordValidator.checkPwdDigit;
 import static edu.uw.main.PasswordValidator.checkPwdLength;
-import static edu.uw.main.PasswordValidator.checkPwdNull;
+import static edu.uw.main.PasswordValidator.checkPwdLowerCase;
 import static edu.uw.main.PasswordValidator.checkPwdSpecialChar;
 import static edu.uw.main.PasswordValidator.checkPwdUpperCase;
 
@@ -28,83 +33,124 @@ import static edu.uw.main.PasswordValidator.checkPwdUpperCase;
 public class RegisterFragment extends Fragment {
 
     private FragmentRegisterBinding binding;
-    private PasswordValidator validate = checkPwdSpecialChar()
-                                         .and(checkExcludeWhiteSpace())
-                                         .and(checkPwdNull());
-    private PasswordValidator pwdValidate = checkExcludeWhiteSpace()
-                                            .and(checkPwdNull())
-                                            .and(checkPwdLength(5))
-                                            .and(checkPwdUpperCase())
-                                            .and(checkClientPredicate
-                                                    (pwd -> pwd.equals(binding.textRepaswd.getText().toString())));
 
+    private RegisterViewModel mRegisterModel;
+
+    private PasswordValidator mNameValidator = checkPwdLength(1);
+
+    private PasswordValidator mEmailValidator = checkPwdLength(2)
+            .and(checkExcludeWhiteSpace())
+            .and(checkPwdSpecialChar("@"));
+
+    private PasswordValidator mPassWordValidator =
+            checkClientPredicate(pwd -> pwd.equals(binding.textRepaswd.getText().toString()))
+                    .and(checkPwdLength(7))
+                    .and(checkPwdSpecialChar())
+                    .and(checkExcludeWhiteSpace())
+                    .and(checkPwdDigit())
+                    .and(checkPwdLowerCase().or(checkPwdUpperCase()));
 
     public RegisterFragment() {
         // Required empty public constructor
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mRegisterModel = new ViewModelProvider(getActivity())
+                .get(RegisterViewModel.class);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        binding = FragmentRegisterBinding.inflate(inflater, container, false);
-        // Inflate the layout for this fragment
+        binding = FragmentRegisterBinding.inflate(inflater);
         return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.buttonSuccess.setOnClickListener(button ->
-                processEmail(binding.textEmail.getText().toString()));
+
+        binding.buttonSuccess.setOnClickListener(this::attemptRegister);
+        mRegisterModel.addResponseObserver(getViewLifecycleOwner(),
+                this::observeResponse);
+    }
+
+    private void attemptRegister(final View button) {
+        validateFirst();
+    }
+
+    private void validateFirst() {
+        mNameValidator.processResult(
+                mNameValidator.apply(binding.textFirstName.getText().toString().trim()),
+                this::validateLast,
+                result -> binding.textEmail.setError("Please enter a valid Email address."));
+    }
+
+    private void validateLast() {
+        mNameValidator.processResult(
+                mNameValidator.apply(binding.textLastName.getText().toString().trim()),
+                this::validateEmail,
+                result -> binding.textEmail.setError("Please enter a valid Email address."));
+    }
+
+    private void validateEmail() {
+        mEmailValidator.processResult(
+                mEmailValidator.apply(binding.textEmail.getText().toString().trim()),
+                this::validatePassword,
+                result -> binding.textEmail.setError("Please enter a valid Email address."));
+    }
+
+    private void validatePassword() {
+        mPassWordValidator.processResult(
+                mPassWordValidator.apply(binding.textPassword.getText().toString()),
+                this::verifyAuthWithServer,
+                result -> binding.textPassword.setError("Please enter a valid Password."));
+    }
+
+    private void verifyAuthWithServer() {
+        mRegisterModel.connect(
+                binding.textFirstName.getText().toString(),
+                binding.textLastName.getText().toString(),
+                binding.textEmail.getText().toString(),
+                binding.textPassword.getText().toString());
+        //This is an Asynchronous call. No statements after should rely on the
+        //result of connect()
+    }
+
+    private void navigateToLogin() {
+        RegisterFragmentDirections.ActionRegisterFragmentToLoginFragment directions =
+                RegisterFragmentDirections.actionRegisterFragmentToLoginFragment();
+        directions.setEmail(binding.textEmail.getText().toString());
+        directions.setPassword(binding.textPassword.getText().toString());
+
+        Navigation.findNavController(getView()).navigate(directions);
 
     }
 
-    public void processEmail(String message){
-        validate.processResult(validate
-                        .apply(binding.textEmail.getText().toString())
-                        .filter(result -> result != PasswordValidator.ValidationResult.SUCCESS),
-                this::processSuccess,
-                this::handleEmailError);
-
-    }
-    private void handleEmailError(PasswordValidator.ValidationResult result) {
-        String report = errorCode(result);
-        binding.textEmail.setError(report);
-    }
-    private void processSuccess(){
-        pwdValidate.processResult(pwdValidate
-                        .apply(binding.textPassword.getText().toString())
-                        .filter(result -> result != PasswordValidator.ValidationResult.SUCCESS),
-                this::success,
-                this::handleSuccessError);
-    }
-    private void handleSuccessError(PasswordValidator.ValidationResult result) {
-        String report = errorCode(result);
-        binding.textPassword.setError(report);
-    }
-    private void success() {
-
-        String message = binding.textEmail.getText().toString();
-        Navigation.findNavController(getView()).navigate(
-                RegisterFragmentDirections.actionRegisterFragmentToSuccessFragment(message, " ")
-        );
-    }
-    private String errorCode(PasswordValidator.ValidationResult result){
-        switch(result){
-            case PWD_MISSING_SPECIAL :
-                return "Invalid: Email missing @ symbol.";
-            case PWD_INCLUDES_WHITESPACE:
-            case PWD_MISSING_FIELD:
-                return "Invalid: Field is empty or contains whitespace in text.";
-            case PWD_INVALID_LENGTH:
-                return "Invalid: Password is less than 6 characters.";
-            case PWD_MISSING_UPPER:
-                return "Invalid: Password does not contain upper case letter.";
-            case PWD_CLIENT_ERROR:
-                return "Invalid: Passwords does not match each other.";
+    /**
+     * An observer on the HTTP Response from the web server. This observer should be
+     * attached to SignInViewModel.
+     *
+     * @param response the Response from the server
+     */
+    private void observeResponse(final JSONObject response) {
+        if (response.length() > 0) {
+            if (response.has("code")) {
+                try {
+                    binding.textEmail.setError(
+                            "Error Authenticating: " +
+                                    response.getJSONObject("data").getString("message"));
+                } catch (JSONException e) {
+                    Log.e("JSON Parse Error", e.getMessage());
+                }
+            } else {
+                navigateToLogin();
+            }
+        } else {
+            Log.d("JSON Response", "No Response");
         }
-        return "ERROR: NO VALID REASON FOUND!";
-    }
 
+    }
 }
