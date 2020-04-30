f0557cd7d951f541e12c1fe83939e07e036c3455
tjack957 <58753664+tjack957@users.noreply.github.com>
2020-04-30 15:09:00 -0700

    Added a mock group chat in the main chat page and added a mock group chat session.

--- app/src/main/java/edu/uw/main/ui/chat/ChatFragment.java
+++ app/src/main/java/edu/uw/main/ui/chat/ChatFragment.java
@@ -2,20 +2,24 @@ package edu.uw.main.ui.chat;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import edu.uw.main.R;
+import edu.uw.main.databinding.FragmentChatBinding;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ChatFragment extends Fragment {
-
+    private FragmentChatBinding binding;
     public ChatFragment() {
         // Required empty public constructor
     }
@@ -24,7 +28,18 @@ public class ChatFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        binding = FragmentChatBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        binding.buttonGroup.setOnClickListener(button ->
+                processFriend());
+    }
+    public void processFriend(){
+        Navigation.findNavController(getView()).navigate(
+                ChatFragmentDirections.actionNavigationChatToGroupFragment()
+        );
     }
 }
