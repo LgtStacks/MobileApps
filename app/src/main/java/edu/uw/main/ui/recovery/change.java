5f525fc07a1e30d591f741b0cc5dd0a9a8f72ab1
Brian Huang <brianhuangoff@gmail.com>
2020-05-16 16:53:52 -0700

    Change password viewmodel

---
+++ app/src/main/java/edu/uw/main/ui/recovery/change.java
@@ -0,0 +1,62 @@
+package edu.uw.main.ui.recovery;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import androidx.lifecycle.ViewModelProvider;
+import edu.uw.main.R;
+import edu.uw.main.databinding.FragmentChangeBinding;
+import edu.uw.main.model.UserInfoViewModel;
+import edu.uw.main.ui.weather.WeatherViewModel;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class change extends Fragment {
+
+    private FragmentChangeBinding  binding;
+
+    private ChangePasswordViewModel mModel;
+
+    public change() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentChangeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        mModel = new ViewModelProvider(getActivity())
+                .get(ChangePasswordViewModel.class);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        UserInfoViewModel model = new ViewModelProvider(getActivity()).get(UserInfoViewModel.class);
+        binding.buttonChange.setOnClickListener(button ->
+                mModel.connect(binding.textChangePw.getText().toString(), model.getmJwt()));
+    }
+
+   // private void
+}
