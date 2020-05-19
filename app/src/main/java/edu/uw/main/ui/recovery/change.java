090d5350b61801eff58169656f64f7deea2701e6
mikeflyuwt <56419237+mikeflyuwt@users.noreply.github.com>
2020-05-19 00:06:19 -0700

    Update change.java
    Updated with errors for incorrect original pw, new pws not matching, new pw format incorrect.

--- app/src/main/java/edu/uw/main/ui/recovery/change.java
+++ app/src/main/java/edu/uw/main/ui/recovery/change.java
@@ -7,9 +7,11 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,10 +20,16 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 import edu.uw.main.AuthActivity;
 import edu.uw.main.MainActivity;
+import edu.uw.main.PasswordValidator;
 import edu.uw.main.R;
 import edu.uw.main.databinding.FragmentChangeBinding;
 import edu.uw.main.model.UserInfoViewModel;
+import edu.uw.main.ui.auth.Login.LoginViewModel;
 import edu.uw.main.ui.weather.WeatherViewModel;
+
+import static edu.uw.main.PasswordValidator.checkExcludeWhiteSpace;
+import static edu.uw.main.PasswordValidator.checkPwdLength;
+import static edu.uw.main.PasswordValidator.checkPwdSpecialChar;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -32,10 +40,12 @@ public class change extends Fragment {
 
     private ChangePasswordViewModel mModel;
 
+    private ChangePasswordViewModel mChangeModel;
+    private UserInfoViewModel mUserViewModel;
+
     public change() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -51,17 +61,85 @@ public class change extends Fragment {
         super.onCreate(savedInstanceState);
         mModel = new ViewModelProvider(getActivity())
                 .get(ChangePasswordViewModel.class);
+
+        mChangeModel = new ViewModelProvider(getActivity())
+                .get(ChangePasswordViewModel.class);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
+        ((MainActivity) getActivity()).setActionBarTitle("Change Password");
         UserInfoViewModel model = new ViewModelProvider(getActivity()).get(UserInfoViewModel.class);
+
+        mChangeModel.addResponseObserver(
+                getViewLifecycleOwner(),
+                this::observeSignInResponse);
+
         binding.buttonChange.setOnClickListener(button -> navigateBack(model));
     }
 
-   private void navigateBack(UserInfoViewModel model) {
-       mModel.connect(binding.textNewPw.getText().toString(),binding.textOldPw.getText().toString(), model.getmJwt());
-       getActivity().onBackPressed();
-   }
+    /**
+     * On success, navigates the user back to home screen.
+     * Returns error on incorrect password formatting and matching.
+     * @param model the UserInfoViewModel
+     */
+    private void navigateBack(UserInfoViewModel model) {
+        if (!binding.textNewPw.getText().toString().equals(binding.textNewRetype.getText().toString())) {
+            binding.textNewPw.setError("Passwords do not match");
+        } else if (binding.textNewPw.length() < 1) {
+            binding.textNewPw.setError("Password field is empty");
+        } else {
+            mModel.connect(binding.textNewPw.getText().toString(), binding.textOldPw.getText().toString(), model.getmJwt());
+            getActivity().onBackPressed();
+        }
+    }
+
+    /**
+     * Method to inform user that they have entered their original password incorrectly.
+     */
+    private void processToastPassword() {
+        Toast toast = Toast.makeText(getActivity(), "Your original password is incorrect" , Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.BOTTOM, 0, 0);
+        toast.show();
+    }
+
+    /**
+     * An observer on the HTTP Response from the web server. This observer should be
+     * attached to ChangePasswordViewModel.
+     *
+     * @param response the Response from the server
+     */
+    private void observeSignInResponse(final JSONObject response) {
+        if (response.length() > 0) {
+            if (response.has("code")) {
+                try {
+                    binding.textOldPw.setError(
+                            "Error Authenticating: " +
+                                    response.getJSONObject("data").getString("message"));
+
+                } catch (JSONException e) {
+                    Log.e("JSON1 Parse Error", e.getMessage());
+                    if (e.getMessage().contains("Passwords")) {
+                        processToastPassword();
+                    }
+                }
+            } else {
+                try {
+                    mUserViewModel = new ViewModelProvider(getActivity(),
+                            new UserInfoViewModel.UserInfoViewModelFactory(
+                                    binding.textOldPw.getText().toString(),
+                                    response.getString("token")
+                            )).get(UserInfoViewModel.class);
+
+
+                } catch (JSONException e) {
+                    Log.e("JSON2 Parse Error", e.getMessage());
+
+                }
+            }
+        } else {
+            Log.d("JSON Response", "No Response");
+        }
+    }
 }
