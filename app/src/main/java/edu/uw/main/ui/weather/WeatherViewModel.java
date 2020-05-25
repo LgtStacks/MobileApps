a113ab714b32ae9434c9b38b35035bd75f2ccd20
mikeflyuwt <56419237+mikeflyuwt@users.noreply.github.com>
2020-05-24 17:09:58 -0700

    Update WeatherViewModel.java
    Update for location tracking

--- app/src/main/java/edu/uw/main/ui/weather/WeatherViewModel.java
+++ app/src/main/java/edu/uw/main/ui/weather/WeatherViewModel.java
@@ -1,12 +1,14 @@
 package edu.uw.main.ui.weather;
 
 import android.app.Application;
+import android.location.Location;
 import android.util.Base64;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 
@@ -28,6 +30,7 @@ import edu.uw.main.io.RequestQueueSingleton;
 public class WeatherViewModel extends AndroidViewModel {
 
     private MutableLiveData<JSONObject> mResponse;
+    private MutableLiveData<Location> mLocation;
 
     /**
      * Standard constructor with parameters.
@@ -37,6 +40,7 @@ public class WeatherViewModel extends AndroidViewModel {
         super(application);
         mResponse = new MutableLiveData<>();
         mResponse.setValue(new JSONObject());
+        mLocation = new MediatorLiveData<>();
     }
 
     /**
@@ -49,9 +53,24 @@ public class WeatherViewModel extends AndroidViewModel {
         mResponse.observe(owner, observer);
     }
 
+    public void addLocationObserver(@NonNull LifecycleOwner owner,
+                                    @NonNull Observer<? super Location> observer) {
+        mLocation.observe(owner, observer);
+    }
+
+    /**
+     *
+     * @param location
+     */
+    public void setLocation(final Location location) {
+        if (!location.equals(mLocation.getValue())) {
+            mLocation.setValue(location);
+        }
+    }
+
     public void connectCurrent(String jwt) {
         String url =
-                "https://app-backend-server.herokuapp.com/weather/current/?q=98402";
+                "https://app-backend-server.herokuapp.com/weather/current/?q=98504";
         Request request = new JsonObjectRequest(
                 Request.Method.POST,
                 url,
@@ -102,7 +121,34 @@ public class WeatherViewModel extends AndroidViewModel {
     public void connectHourly(String jwt) {
         String url =
                 "https://app-backend-server.herokuapp.com/weather/hourly/?postal_code=98502&country=US";
-                //"https://app-backend-server.herokuapp.com/weather/hourly";
+               // "https://app-backend-server.herokuapp.com/weather/hourly";
+        Request request = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null, //no body for this get request
+                mResponse::setValue,
+                this::handleError) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                // add headers <key,value>
+                headers.put("Authorization", jwt);
+                return headers;
+            }
+        };
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                10_000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        //Instantiate the RequestQueue and add the request to the queue
+        RequestQueueSingleton.getInstance(getApplication().getApplicationContext())
+                .addToRequestQueue(request);
+    }
+
+    public void connectLatLon(String jwt, double lat, double lon) {
+        String url =
+                //"https://app-backend-server.herokuapp.com/weather/hourly/?lat=" + lat + "?lon=" + lon;
+                "https://app-backend-server.herokuapp.com/weather/hourly/?&lat=" + lat + "&lon=" + lon;
         Request request = new JsonObjectRequest(
                 Request.Method.GET,
                 url,
