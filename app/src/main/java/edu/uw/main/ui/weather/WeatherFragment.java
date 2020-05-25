04ccfb6334cd6a5bb279ebad556e2d7f8f56b1ff
mikeflyuwt <56419237+mikeflyuwt@users.noreply.github.com>
2020-05-25 13:33:14 -0700

    Update WeatherFragment.java
    Local weather displayed for current, hourly, and forecast

--- app/src/main/java/edu/uw/main/ui/weather/WeatherFragment.java
+++ app/src/main/java/edu/uw/main/ui/weather/WeatherFragment.java
@@ -91,50 +91,35 @@ public class WeatherFragment extends Fragment {
         Log.e("Hour of Day", String.valueOf(mHour));
         Log.e("Hour", String.valueOf(mHour2));
         UserInfoViewModel model = new ViewModelProvider(getActivity()).get(UserInfoViewModel.class);
-//        binding.buttonCurrent.setOnClickListener(button ->
-//                handleCurrent(model.getmJwt()));
-//        binding.buttonForecast.setOnClickListener(button ->
-//                handleForecast(model.getmJwt()));
-//        binding.buttonHourly.setOnClickListener(button ->
-//                handleHourly(model.getmJwt()));
+
         mWeatherModel.addResponseObserver(
                 getViewLifecycleOwner(),
                 this::observeWeatherResponse);
 
+
         FragmentWeatherBinding binding = FragmentWeatherBinding.bind(getView());
 
         mWeatherModel = new ViewModelProvider(getActivity()).get(WeatherViewModel.class);
+
         mWeatherModel.addLocationObserver(getViewLifecycleOwner(),
-                location -> mWeatherModel.connectCurrent(model.getmJwt()));
+                location -> {
+                    if (location != null) {
+                        mWeatherModel.connectLatLon(model.getmJwt(), location.getLatitude(), location.getLongitude());
+                    }
+                });
+
         mWeatherModel.addLocationObserver(getViewLifecycleOwner(),
-                location -> mWeatherModel.connectHourly(model.getmJwt()));
-        mWeatherModel.addLocationObserver(getViewLifecycleOwner(),
-                location -> mWeatherModel.connectForecast(model.getmJwt()));
+                location -> {
+                    if (location != null) {
+                        mWeatherModel.connectLatLonForecast(model.getmJwt(), location.getLatitude(), location.getLongitude());
+                    }
+                });
 
         binding.button.setOnClickListener(button ->
                 Navigation.findNavController(getView()).
                         navigate(WeatherFragmentDirections.actionWeatherActivityToLocationFragment()));
 
     }
-
-//    public void handleCurrent(String jwt){
-//
-//      //  mWeatherModel.connectCurrent(jwt);
-//
-//        binding.layoutWait.setVisibility(View.VISIBLE);
-//    }
-//    public void handleForecast(String jwt){
-//
-//        mWeatherModel.connectForecast(jwt);
-//
-//        binding.layoutWait.setVisibility(View.VISIBLE);
-//    }
-//    public void handleHourly(String jwt){
-//
-//        mWeatherModel.connectHourly(jwt);
-//
-//        binding.layoutWait.setVisibility(View.VISIBLE);
-//    }
 
     private void observeWeatherResponse(final JSONObject response) {
         if (response.length() > 0) {
@@ -146,64 +131,40 @@ public class WeatherFragment extends Fragment {
                 } catch (JSONException e) {
                     Log.e("JSON1 Parse Error", e.getMessage());
                 }
-            } else {
-                if(response.has("list")){
-                    parseForecast(response);
-                    Log.e("Button Pressed", "Forecast");
-                }
-                else if(response.has("data")){
-                    parseHourly(response);
-                    Log.e("Button Pressed", "Hourly");
-                }
-                else {
-                    parseCurrent(response);
-                    Log.e("Button Pressed", "Current");
-                }
-                //binding.textWeather.setText(response.toString());
-                Log.e("Response", response.toString());
+            } else if (response.has("data")) {
+                parseForecast(response);
+                Log.e("ForecastData", "Forecast");
+                parseHourly(response);
+                Log.e("HourlyData", "Hourly");
             }
         } else {
             Log.d("JSON Response", "No Response");
         }
     }
     private void parseForecast(final JSONObject response){
+        Log.d("Forecast!", "gotten");
+        String date = "";
+        float highTemp = 0;
+        float lowTemp = 0;
+        String descrip = "";
+        String whole = "";
         try {
-            int counter = 1;
-            int maxTemp = 0;
-            int minTemp = 10000000;
-            String whole = "";
-            String date = "";
-            String weather = "";
-            JSONArray jsTemp = response.getJSONArray("list");
-            for(int i = 0; i < 40; i++){
-                JSONObject currObj = jsTemp.getJSONObject(i);
-                if(counter > 7){
-                    String theString = date + ": High: " + kelvinToFahrenheit(maxTemp) + "\u00B0 F"
-                            + " Low: " + kelvinToFahrenheit(minTemp) + "\u00B0 F"
-                            + " Weather: " + weather + "\n";
-                    counter = 0;
-                    maxTemp = 0;
-                    minTemp = 100000000;
-                    whole += theString;
-                }
-                if(counter == 1) {
-                    weather = currObj.getJSONArray("weather")
-                            .getJSONObject(0)
-                            .get("description")
-                            .toString();
-                    date = currObj.get("dt_txt").toString().split(" ")[0];
-                }
-                int currentMax = currObj.getJSONObject("main").getInt("temp_max");
-                Log.e("MAX", String.valueOf(currentMax));
-                int currentMin =currObj.getJSONObject("main").getInt("temp_min");
-                Log.e("MIN", String.valueOf(currentMin));
-                if(currentMax > maxTemp) {
-                    maxTemp = currentMax;
-                }
-                if(currentMin < minTemp) {
-                    minTemp = currentMin;
-                }
-                counter++;
+            for (int i = 0; i < 9; i++) {
+                JSONArray jsTemp = response.getJSONArray("data");
+
+                date = jsTemp.getJSONObject(i).get("valid_date").toString();
+
+                highTemp = Float.parseFloat(jsTemp.getJSONObject(i).get("high_temp").toString());
+
+                lowTemp = Float.parseFloat(jsTemp.getJSONObject(i).get("app_min_temp").toString());
+
+               descrip = jsTemp.getJSONObject(i).getJSONObject("weather").get("description").toString();
+
+                String theString = date
+                        + " High " + celsiusToFahrenheit(highTemp) + "°F "
+                        + " Low " + celsiusToFahrenheit(lowTemp) + "°F "
+                        + descrip + "\n";
+                whole += theString;
             }
             binding.textWeatherForecast.setText(whole);
         } catch (JSONException e) {
@@ -216,9 +177,32 @@ public class WeatherFragment extends Fragment {
         String date = "";
         float temp = 0;
         String descrip = "";
+        String city = "";
+        String state = "";
+        String country = "";
+        String part = "";
         String whole = "";
         try {
-            for (int i =  0; i < 24; i++) {
+            for (int i =  0; i < 1; i++) {
+                JSONArray jsTemp = response.getJSONArray("data");
+
+                temp = Float.parseFloat(jsTemp.getJSONObject(i).get("temp").toString());
+
+                descrip = jsTemp.getJSONObject(i).getJSONObject("weather").get("description").toString();
+
+                city = response.getString("city_name");
+
+                state = response.getString("state_code");
+
+                country = response.getString("country_code");
+
+                String theString = "Current temperature: " + celsiusToFahrenheit(temp) + "°F\n"
+                        + descrip + "\n" + city + ", " + state + " " + country + "\n";
+                part += theString;
+            }
+            binding.textWeatherCurrent.setText(part);
+
+            for (int i =  1; i < 24; i++) {
                 JSONArray jsTemp = response.getJSONArray("data");
 
                 date = jsTemp.getJSONObject(i).get("timestamp_local").toString();
@@ -238,38 +222,9 @@ public class WeatherFragment extends Fragment {
             Log.e("JSON1 Parse Error", e.getMessage());
         }
     }
-    private void parseCurrent(final JSONObject response){
-        try {
-            String weather = response.getJSONArray("weather").getJSONObject(0).get("description").toString();
-            int temp = response.getJSONObject("main").getInt("temp");
-            int feels_like =response.getJSONObject("main").getInt("feels_like");
-            int pressure =response.getJSONObject("main").getInt("pressure");
-            int humidity =response.getJSONObject("main").getInt("humidity");
-            int sunrise = response.getJSONObject("sys").getInt("sunrise");
-            int sunset = response.getJSONObject("sys").getInt("sunset");
-            String whole = "Weather: " + weather + "\n"
-                    + "Temp: " + kelvinToFahrenheit(temp) + "\u00B0 F\n"
-                    + "Feels Like: " + kelvinToFahrenheit(feels_like) + "\u00B0 F\n"
-                    + "Pressure: " + (pressure * 100) / 6895 + " PSI\n"
-                    + "Humidity: " + humidity + "%\n"
-                    + "Sunrise: " + getDate(sunrise) + " AM\n"
-                    + "Sunset: " + getDate(sunset) + " PM\n";
-            binding.textWeatherCurrent.setText(whole);
-        }
-        catch (JSONException e) {
-            Log.e("JSON1 Parse Error", e.getMessage());
-        }
-    }
-
-    private double kelvinToFahrenheit(int k){
-        double temp = (k-273.15)* (9/5) + 32;
-        return Math.floor(temp * 100) / 100;
-    }
-    private String getDate(long time) {
-        Calendar cal = Calendar.getInstance(Locale.ENGLISH);
-        cal.setTimeInMillis(time * 1000);
-        String date = DateFormat.format("hh:mm:ss", cal).toString();
-        return date;
+    private int celsiusToFahrenheit(float c){
+        int temp = (int) ((c/5) * 9 + 32);
+        return temp;
     }
     @Override
     public void onResume() {
