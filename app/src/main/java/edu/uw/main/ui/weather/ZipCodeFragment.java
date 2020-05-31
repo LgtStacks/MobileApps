1177e877ed81b1f9102830c275b7a499c0108111
mikeflyuwt <56419237+mikeflyuwt@users.noreply.github.com>
2020-05-31 15:25:24 -0700

    Update ZipCodeFragment.java
    Weather/ZipCode update

--- app/src/main/java/edu/uw/main/ui/weather/ZipCodeFragment.java
+++ app/src/main/java/edu/uw/main/ui/weather/ZipCodeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.RemoteInput;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
@@ -18,6 +19,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -57,7 +59,7 @@ public class ZipCodeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ((MainActivity) getActivity())
-                .setActionBarTitle("Search Weather By Zip Code");
+                .setActionBarTitle("Weather");
         UserInfoViewModel model = new ViewModelProvider(getActivity()).get(UserInfoViewModel.class);
 
         mWeatherModel.addResponseObserver(
@@ -108,24 +110,59 @@ public class ZipCodeFragment extends Fragment {
      * @param response The JSONObject bundle response.
      */
     private void parseZip(final JSONObject response){
+        String timeStamp = new SimpleDateFormat("HHmmss").format(Calendar.getInstance().getTime());
         try {
             String weather = response.getJSONArray("weather").getJSONObject(0).get("description").toString();
             int temp = response.getJSONObject("main").getInt("temp");
-            int feels_like =response.getJSONObject("main").getInt("feels_like");
-            int pressure =response.getJSONObject("main").getInt("pressure");
-            int humidity =response.getJSONObject("main").getInt("humidity");
+            int feels_like = response.getJSONObject("main").getInt("feels_like");
+            int pressure = response.getJSONObject("main").getInt("pressure");
+            int humidity = response.getJSONObject("main").getInt("humidity");
             int sunrise = response.getJSONObject("sys").getInt("sunrise");
             int sunset = response.getJSONObject("sys").getInt("sunset");
             String city = response.getString("name");
+            String tempTemp = kelvinToFahrenheit(temp) + "\u00B0 F";
             String whole = "Weather: " + weather + "\n"
-                    + "Temp: " + kelvinToFahrenheit(temp) + "\u00B0 F\n"
                     + "Feels Like: " + kelvinToFahrenheit(feels_like) + "\u00B0 F\n"
                     + "Pressure: " + (pressure * 100) / 6895 + " PSI\n"
                     + "Humidity: " + humidity + "%\n"
                     + "Sunrise: " + getDate(sunrise) + " AM\n"
-                    + "Sunset: " + getDate(sunset) + " PM\n"
-                    + city;
+                    + "Sunset: " + getDate(sunset) + " PM\n";
+            binding.textTemp.setText(tempTemp);
+            binding.textCity.setText(city);
             binding.currentWeather.setText(whole);
+            Log.d("Time", timeStamp);
+            boolean dayTime = Integer.parseInt(timeStamp) > 60000 && Integer.parseInt(timeStamp) < 190000;
+            if (weather.equals("clear sky")) {
+                if (dayTime) {
+                    binding.imageView5.setImageResource(R.drawable.dayclearsky);
+                } else {
+                    binding.imageView5.setImageResource(R.drawable.nightclearsky);
+                }
+            } else if (weather.equals("few clouds")) {
+                if (dayTime) {
+                    binding.imageView5.setImageResource(R.drawable.dayfewclouds);
+                } else {
+                    binding.imageView5.setImageResource(R.drawable.nightfewclouds);
+                }
+            } else if (weather.equals("scattered clouds")) {
+                binding.imageView5.setImageResource(R.drawable.daynightscatteredclouds);
+            } else if (weather.equals("broken clouds") || weather.equals("overcast clouds")) {
+                binding.imageView5.setImageResource(R.drawable.daynightbrokenclouds);
+            } else if (weather.equals("shower rain")) {
+                binding.imageView5.setImageResource(R.drawable.daynightshowerrain);
+            } else if (weather.equals("mist")) {
+                binding.imageView5.setImageResource(R.drawable.daynightmist);
+            } else if (weather.equals("rain")) {
+                if (dayTime) {
+                    binding.imageView5.setImageResource(R.drawable.dayrain);
+                } else {
+                    binding.imageView5.setImageResource(R.drawable.nightrain);
+                }
+            } else if (weather.equals("thunderstorm")) {
+                binding.imageView5.setImageResource(R.drawable.daynightthunderstorm);
+            } else if (weather.equals("snow")) {
+                binding.imageView5.setImageResource(R.drawable.daynightsnow);
+            }
         }
         catch (JSONException e) {
             Log.e("JSON1 Parse Error", e.getMessage());
